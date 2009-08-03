382b0ad643b419b48e5027c2e77a00b734da6b24
btknorr <btknorr@63d4da46-1340-11de-a72a-edd3f5372759>
2009-08-03 02:45:10 +0000

    Big refactoring...broke out code more...added redo support...changed interface some, so client test code may need to be updated.
    git-svn-id: http://uispec.googlecode.com/svn/trunk@34 63d4da46-1340-11de-a72a-edd3f5372759

--- xcode/UISpecDemo/SpecMain.m
+++ xcode/UISpecDemo/SpecMain.m
@@ -4,8 +4,8 @@
 int main(int argc, char *argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	
-	//[UISpec runSpecsAfterDelay:3];
-    [UISpec runSpec:@"DescribeEmployeeAdmin" example:@"itShouldUpdateUserRoles" afterDelay:1];
+	[UISpec runSpecsAfterDelay:3];
+    //[UISpec runSpec:@"DescribeEmployeeAdmin" example:@"itShouldUpdateUserRoles" afterDelay:1];
 	
 	int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
